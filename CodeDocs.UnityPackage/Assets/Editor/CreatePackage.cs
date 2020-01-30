4cab3c052de4aebd9323c9130713746632f1bb67
zbyhoo <zbyhoo@gmail.com>
2020-01-30 12:12:55 +0100

    moving executable to seperate folder

--- CodeDocs.UnityPackage/Assets/Editor/CreatePackage.cs
+++ CodeDocs.UnityPackage/Assets/Editor/CreatePackage.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public static class CreatePackage
 {
+    private static string DocsRootDestinationPath =>
+        Path.Combine(Application.dataPath, "Gamesture.CodeDocs", "Editor", "CodeDocs");
+    
     [MenuItem("Gamesture/Export Package", false, 20)]
     public static void Export()
     {
@@ -20,8 +23,38 @@ public static class CreatePackage
         {
             File.Delete(destination);
         }
+        
+        CopyDocsRoot();
+        AssetDatabase.Refresh();
 
         AssetDatabase.ExportPackage(SOURCE, destination,
             ExportPackageOptions.IncludeDependencies | ExportPackageOptions.Recurse);
+
+        RemoveDocsRoot();
+        AssetDatabase.Refresh();
+    }
+
+    private static void RemoveDocsRoot()
+    {
+        Directory.Delete(DocsRootDestinationPath, true);
+        File.Delete($"{DocsRootDestinationPath}.meta");
+    }
+
+    private static void CopyDocsRoot()
+    {
+        string sourcePath = Path.Combine(Application.dataPath, "..", "CodeDocs");
+        string destinationPath = DocsRootDestinationPath;
+        
+        // create all of the directories
+        foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+        {
+            Directory.CreateDirectory(dirPath.Replace(sourcePath, destinationPath));
+        }
+
+        // copy all the files & Replaces any files with the same name
+        foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+        {
+            File.Copy(newPath, newPath.Replace(sourcePath, destinationPath), true);
+        }
     }
 }
