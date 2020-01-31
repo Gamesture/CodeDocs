a81adc7b2d7bb3899cba3ab0c0298bde0b1f4302
zbyhoo <zbyhoo@gmail.com>
2020-01-31 09:13:01 +0100

    making temporary files invisible to Unity Editor

--- CodeDocs.UnityPackage/Assets/Gamesture.CodeDocs/Editor/Executable/generate.bat
+++ CodeDocs.UnityPackage/Assets/Gamesture.CodeDocs/Editor/Executable/generate.bat
@@ -3,15 +3,15 @@ setlocal EnableExtensions EnableDelayedExpansion
 
 cd /D "%~dp0"
 del "GeneratedDocs~\html\index.html" >nul 2>&1
-del "doxygen_mod" >nul 2>&1
+del "doxygen_mod~" >nul 2>&1
 
 for /F "usebackq tokens=*" %%x in (Doxyfile) do (
     set line=%%x
     set product=!line:_gamesture_product_=%1%!
     set final=!product:_gamesture_sources_=%2%!
 
-    echo !final!>>doxygen_mod
+    echo !final!>>doxygen_mod~
 )
 
-doxygen.exe doxygen_mod
-del "doxygen_mod" >nul 2>&1
+doxygen.exe doxygen_mod~
+del "doxygen_mod~" >nul 2>&1
--- CodeDocs.UnityPackage/Assets/Gamesture.CodeDocs/Editor/Executable/generate.sh
+++ CodeDocs.UnityPackage/Assets/Gamesture.CodeDocs/Editor/Executable/generate.sh
@@ -7,9 +7,9 @@ command -v doxygen >/dev/null 2>&1 || { echo >&2 "I require doxygen but it's not
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${script_dir}
-sed s~_gamesture_product_~${1}~g Doxyfile > Doxyfile_product
-sed s~_gamesture_sources_~${2}~g Doxyfile_product > Doxyfile_mod
-rm Doxyfile_product
+sed s~_gamesture_product_~${1}~g Doxyfile > Doxyfile_product~
+sed s~_gamesture_sources_~${2}~g Doxyfile_product~ > Doxyfile_mod~
+rm Doxyfile_product~
 rm -rf GeneratedDocs~
-doxygen Doxyfile_mod
-rm Doxyfile_mod
+doxygen Doxyfile_mod~
+rm Doxyfile_mod~
