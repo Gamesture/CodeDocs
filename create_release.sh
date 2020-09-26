ebfbb8ced74bbd5ba0e18a145b4069cafd151a8c
zbyhoo <zbyhoo@gmail.com>
2020-09-26 18:04:23 +0200

    changing script for version creation

--- create_release.sh
+++ create_release.sh
@@ -1,5 +1,14 @@
 #/bin/bash
 
+function check_last_error_code
+{
+    error_code="${?}"
+    if [ $error_code -ne 0 ]; then
+        echo " ERROR! (code: ${error_code})"
+        exit $error_code
+    fi
+}
+
 version=$1
 
 if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
@@ -13,10 +22,11 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${script_dir}
 
 find ./CodeDocs.UnityPackage/Assets/Gamesture.CodeDocs -name package.json -exec sed -i '' 's/"version": "[0-9]*\.[0-9]*\.[0-9]*"/"version": "'$1"\""/g {} \;
-git add -A
+git add ./CodeDocs.UnityPackage/Assets/Gamesture.CodeDocs/package.json
+check_last_error_code
 git commit -m "version $1"
+check_last_error_code
 git push
-
-git subtree split --prefix=CodeDocs.UnityPackage/Assets/Gamesture.CodeDocs --branch upm
-git tag $1 upm
-git push origin upm
+check_last_error_code
+cd ./CodeDocs.UnityPackage/Assets/Gamesture.CodeDocs && npm publish --registry http://34.107.237:4873 && cd -
+check_last_error_code
