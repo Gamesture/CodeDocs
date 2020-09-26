a3ec12d24e5c9dacec35dacc9f8e25952ce537f1
zbyhoo <zbyhoo@gmail.com>
2020-09-26 17:13:32 +0200

    adding script for version change and change to version 0.2.0

---
+++ CodeDocs.UnityPackage/Assets/Editor/VersionEditor.cs
@@ -0,0 +1,74 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+
+public class VersionEditor : EditorWindow
+{
+    private class PackageFormat
+    {
+        public string version;
+    }
+
+    private int _major;
+    private int _minor;
+    private int _patch;
+
+    [MenuItem("Gamesture/Build/Set Version", false, 10000)]
+    public static void SetVersion()
+    {
+        VersionEditor window =
+            (VersionEditor) GetWindow(typeof(VersionEditor), false, "Game Version");
+        window.SetDefaultVersion();
+        window.Show();
+    }
+
+    private static string GetCurrentVersion()
+    {
+        string versionScriptPath = Path.Combine(Application.dataPath, "Gamesture.CodeDocs", "package.json");
+        string content = File.ReadAllText(versionScriptPath);
+        return JsonUtility.FromJson<PackageFormat>(content).version;
+    }
+
+    private void SetDefaultVersion()
+    {
+        string[] versionParts = GetCurrentVersion().Split('.');
+        _major = int.Parse(versionParts[0]);
+        _minor = int.Parse(versionParts[1]);
+        _patch = int.Parse(versionParts[2]);
+    }
+
+    public void OnGUI()
+    {
+        _major = EditorGUILayout.IntField("major: ", _major);
+        _minor = EditorGUILayout.IntField("minor: ", _minor);
+        _patch = EditorGUILayout.IntField("patch: ", _patch);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField($"VERSION: {_major}.{_minor}.{_patch}");
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Set"))
+        {
+            UpdateVersion();
+        }
+    }
+
+    private void UpdateVersion()
+    {
+        string version = $"{_major}.{_minor}.{_patch}";
+        UpdateVersionFile(version);
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    private static void UpdateVersionFile(string version)
+    {
+        string versionScriptPath = Path.Combine(Application.dataPath, "Gamesture.CodeDocs", "package.json");
+        string content = File.ReadAllText(versionScriptPath);
+        PackageFormat packageFormat = JsonUtility.FromJson<PackageFormat>(content);
+        content = content.Replace(packageFormat.version, $"{version}");
+        File.WriteAllText(versionScriptPath, content);
+    }
+}
