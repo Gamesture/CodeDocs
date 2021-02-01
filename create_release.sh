97e3c803e7fd751efaef7956b44e6553bdab96df
zbyhoo <zbyhoo@gmail.com>
2021-02-01 13:36:09 +0100

    testing new npm registry

--- create_release.sh
+++ create_release.sh
@@ -29,5 +29,5 @@ git commit -m "version $1"
 check_last_error_code
 git push
 check_last_error_code
-cd ./CodeDocs.UnityPackage/Assets/Gamesture.CodeDocs && npm publish --registry https://unity-npm.admin-gamesture.com/ && cd -
+cd ./CodeDocs.UnityPackage/Assets/Gamesture.CodeDocs && npm publish --registry https://teamcity.admin-gamesture.com/ && cd -
 check_last_error_code
